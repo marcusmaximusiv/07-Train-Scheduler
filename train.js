c94d2b1f832d8629d6a8b55bf8d6e12b32e336d2
Sterrenberg, Mark <mark.allen.sterrenberg@gmail.com>
2020-01-09 20:35:35 -0600

    adding css to a separate file to make sure the css is separate and can be edited easily

--- train.js
+++ train.js
@@ -1,8 +1,8 @@
 // 1. Initialize Firebase
 var config = {
     apiKey: "AIzaSyB0xjCAbMGZmucV6P3dQfCxJCXZAOjYh2E",
-    authDomain: "time-sheet-55009.firebaseapp.com",
-    databaseURL: "https://time-sheet-55009.firebaseio.com",
+    authDomain: "train-scheduler-bfc09.firebaseapp.com",
+    databaseURL: "https://train-scheduler-bfc09.firebaseio.com/",
     storageBucket: "train-scheduler-bfc09.appspot.com"
   };
   
@@ -10,5 +10,21 @@ var config = {
   
   var database = firebase.database();
 
-//2. Have variables to store the firebase data
-
+//2. Have an onclick event for the adding a train button
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+//3. Have variables to store all of the form data
+var trainname=
+var dest=
+var freq=
+var firsttrain=
+//4. Create a temporary object to hold all the data from the form
+var newTrain = {
+	name: trainname,
+	dest: dest
+	freq: freq,
+	firsttrain: firsttrain
+};
+//5. Push new data to the temporary object
+database.ref().push(newTrain);
+  
